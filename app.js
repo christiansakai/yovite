32975ea8289182fa7578e0f146f4a72ee8708d25
Christian <christ_sapphire@yahoo.com>
2014-07-25 22:56:43 -0400

    finished

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var request = require('request');
 
 var routes = require('./routes/index');
 
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,29 @@ var express = require('express');
 var exec = require('child_process').exec;
 var router = express.Router();
 
-var makeKey_start = "curl \'https://api.parse.com/1/users\' -H \'Origin: http://dev.justyo.co\' -H \'Accept-Encoding: gzip,deflate,sdch\' -H \'Accept-Language: en-US,en;q=0.8\' -H \'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36\' -H \'Content-Type: text/plain\' -H \'Accept: */*\' -H \'Referer: http://dev.justyo.co/\' -H \'Connection: keep-alive\' --data-binary \'{\"username\":"
-
-var makeKey_end = ",\"callback\":\"http://asdf.co\",\"emailAddress\":\"ne2210@columbie.edu\",\"password\":\"59c32cd0-c3d7-e1f5-c76f-990f5f4acbb7\",\"udid\":\"5df934c7-ce11-eb05-c162-01be8c2c5128\",\"parentUser\":\"NEPSTEIN\",\"_ApplicationId\":\"iPmwrhjFVCYwL4ZZlicpCIeyJIbRUuXvPhLMCxOs\",\"_JavaScriptKey\":\"MPSGagRkBsI3OCIfFooumAm7sDHaAzZjDlP8BEDN\",\"_ClientVersion\":\"js1.2.18\",\"_InstallationId\":\"5ffaea5d-b2ff-ccf7-5c7f-7e4514ef5ac4\",\"_SessionToken\":\"CGz1ESoL8qEkl5h59LyfAjSpZ\"}\' --compressed";
-
-
-var child;
-
 router.get('/', function(req, res) {
   res.render('index');
 });
 
 router.post('/', function(req, res) {
+  var hex = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+  var key = "";
+
+  for (var i = 0; i < 32; i++) {
+    var char = hex[Math.floor(Math.random() * 15)];
+    key = key + char;
+
+    if ( i === 7 || i === 11 || i === 15 || i === 19) {
+      key = key + '-';
+    }
+  }
+
+  var makeKey_start = "curl \'https://api.parse.com/1/users\' -H \'Origin: http://dev.justyo.co\' -H \'Accept-Encoding: gzip,deflate,sdch\' -H \'Accept-Language: en-US,en;q=0.8\' -H \'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36\' -H \'Content-Type: text/plain\' -H \'Accept: */*\' -H \'Referer: http://dev.justyo.co/\' -H \'Connection: keep-alive\' --data-binary \'{\"username\":";
+
+  var makeKey_end = ",\"callback\":\"http://asdf.co\",\"emailAddress\":\"ne2210@columbie.edu\",\"password\":\"59c32cd0-c3d7-e1f5-c76f-990f5f4acbb7\",\"udid\":\""+key+"\",\"parentUser\":\"NEPSTEIN\",\"_ApplicationId\":\"iPmwrhjFVCYwL4ZZlicpCIeyJIbRUuXvPhLMCxOs\",\"_JavaScriptKey\":\"MPSGagRkBsI3OCIfFooumAm7sDHaAzZjDlP8BEDN\",\"_ClientVersion\":\"js1.2.18\",\"_InstallationId\":\"5ffaea5d-b2ff-ccf7-5c7f-7e4514ef5ac4\",\"_SessionToken\":\"CGz1ESoL8qEkl5h59LyfAjSpZ\"}\' --compressed";
+
+  var child;
+
   var event = req.body;
 
   console.log('Event submitted: ');
@@ -33,19 +44,42 @@ router.post('/', function(req, res) {
     console.log('\n');
 
     for (var i = 0, l = event.friends.length; i < l; i++) {
-      var initYo = 'curl --data "api_token=5df934c7-ce11-eb05-c162-01be8c2c5128&username='
-                    + event.friends[i].toUpperCase() + '" http://api.justyo.co/yo/';
+      var initYo = 'curl --data "api_token='
+                    + key
+                    + '&username='
+                    + event.friends[i].toUpperCase()
+                    + '" http://api.justyo.co/yo/';
 
-      child = exec(initYo);
+      child = exec(initYo, function(a, b, c) {
+        console.log('init Yo: ');
+        console.log(initYo);
+        console.log(a, b, c);
+        console.log('\n');
+      });
     }
 
     setTimeout(function(){
-      var checkCount = 'curl "http://api.justyo.co/subscribers_count/?api_token=5df934c7-ce11-eb05-c162-01be8c2c5128"';
+      var checkCount = 'curl "http://api.justyo.co/subscribers_count/?api_token=' + key + '"';
 
-      var all = 'curl --data "api_token=5df934c7-ce11-eb05-c162-01be8c2c5128" http://api.justyo.co/yoall/';
+      console.log('check subscribers curl: ');
+      console.log(checkCount);
+      console.log('\n');
 
-      var host = 'curl --data "api_token=5df934c7-ce11-eb05-c162-01be8c2c5128&username='
-                 + event.host.toUpperCase() + '" http://api.justyo.co/yo/';
+      var all = 'curl --data "api_token=' + key + '" http://api.justyo.co/yoall/';
+
+      console.log('yo all curl: ');
+      console.log(all);
+      console.log('\n');
+
+      var host = 'curl --data "api_token='
+                  + key
+                  + '&username='
+                  + event.host.toUpperCase()
+                  + '" http://api.justyo.co/yo/';
+
+      console.log('host curl: ');
+      console.log(host);
+      console.log('\n');
 
       child = exec(checkCount, function(err, stdout, stderr) {
         var subscribed = parseInt(stdout.toString().slice(11));
